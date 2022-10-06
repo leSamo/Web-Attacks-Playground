f7ddddfde90cbd7cf8e8998dac5bed6ad2d047ab
leSamo <soleksak@redhat.com>
2022-10-06 02:06:43 +0200

    Tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import {
   BrowserRouter as Router,
@@ -9,7 +9,8 @@ import Wrapper from './Wrapper';
 import IndexPage from './IndexPage';
 import Xss from './Xss/Xss';
 import Validation from './Validation/Validation';
-import { Card, CardBody, CodeBlock, CodeBlockCode, Split, SplitItem, Text, TextContent } from '@patternfly/react-core';
+import { Button, Card, CardBody, CodeBlock, CodeBlockCode, Split, SplitItem, Tab, Tabs, Text, TextContent } from '@patternfly/react-core';
+import { ArrowRightIcon } from '@patternfly/react-icons';
 
 const App = () => {
   const paths = {
@@ -23,10 +24,11 @@ const App = () => {
       tabName: "XSS (Cross site scripting)",
       component: <Xss />,
       attackDescription: <p>Cross-Site Scripting (XSS) attacks are a type of injection, in which malicious scripts are injected into otherwise benign and trusted websites. XSS attacks occur when an attacker uses a web application to send malicious code, generally in the form of a browser side script, to a different end user.</p>,
-      tryItYourself: <p>This website contains a text area where users can enter a new comment and post it. It supports HTML tags so users can format their comments. Implementation of this feature is vulnerable to XSS attacks, as it does not check which HTML tags are used. In particular, script tag can be used to execute JavaScript code on other users&#39; devices. Paste the following HTML code snippet into new comment text area and submit the comment.<CodeBlock>
-      <CodeBlockCode id="code-content">{"<script>alert(\"XSS attack\")</script>"}</CodeBlockCode>
-    </CodeBlock></p>,
-      howToProtect: <p>The primary defenses against XSS are described in the <a href="https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html" target="_blank" rel="noopener noreferrer">OWASP XSS Prevention Cheat Sheet</a>. Also, it’s crucial that you turn off HTTP TRACE support on all web servers. An attacker can steal cookie data via Javascript even when document.cookie is disabled or not supported by the client.</p>
+      tryItYourself: <p>Try to inject such JavaScipt code into the page that shows a modal window with text &quot;XSS attack&quot;.</p>,
+      solution: <p>This website contains a text area where users can enter a new comment and post it. It supports HTML tags so users can format their comments. Implementation of this feature is vulnerable to XSS attacks, as it does not check which HTML tags are used. In particular, script tag can be used to execute JavaScript code on other users&#39; devices. Paste the following HTML code snippet into new comment text area and submit the comment.<CodeBlock>
+        <CodeBlockCode id="code-content">{"<script>alert(\"XSS attack\")</script>"}</CodeBlockCode>
+      </CodeBlock></p>,
+      howToPrevent: <p>The primary defenses against XSS are described in the <a href="https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html" target="_blank" rel="noopener noreferrer">OWASP XSS Prevention Cheat Sheet</a>. Also, it’s crucial that you turn off HTTP TRACE support on all web servers. An attacker can steal cookie data via Javascript even when document.cookie is disabled or not supported by the client.</p>
     },
     "/crsf": {
       tabName: "CRSF"
@@ -43,6 +45,8 @@ const App = () => {
     }
   }
 
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <Router>
       <Switch>
@@ -51,36 +55,77 @@ const App = () => {
             <IndexPage />
           </Wrapper>
         </Route>
-        {Object.entries(paths).map(([urlPath, { tabName, component, attackDescription, tryItYourself, howToProtect }], index) =>
+        {Object.entries(paths).map(([urlPath, { tabName, component, attackDescription, tryItYourself, solution, howToPrevent }], index) =>
           <Route exact path={urlPath} key={index + 1}>
             <Wrapper navIndex={index + 1} paths={paths}>
               <Split style={{ minHeight: "100%" }}>
-                <SplitItem style={{ width: 400 }}>
+                <SplitItem style={{ width: 500 }}>
                   <Card style={{ minHeight: "100%" }}>
                     <CardBody>
                       <TextContent>
                         <Text component="h2">
                           {tabName}
                         </Text>
-                        <Text component="h3">
-                          About the attack
-                        </Text>
-                        <Text component="p" style={{ textAlign: "justify" }}>
-                          {attackDescription}
-                        </Text>
-                        <Text component="h3">
-                          Try it yourself
-                        </Text>
-                        <Text component="p" style={{ textAlign: "justify" }}>
-                          {tryItYourself}
-                        </Text>
-                        <Text component="h3">
-                          How to protect
-                        </Text>
-                        <Text component="p" style={{ textAlign: "justify" }}>
-                          {howToProtect}
-                        </Text>
                       </TextContent>
+                      <Tabs
+                        activeKey={activeTab}
+                        onSelect={(e, tab) => setActiveTab(tab)}
+                        isBox
+                        style={{ marginTop: 16, marginBottom: 16 }}
+                      >
+                        <Tab eventKey={0} title="Description">
+                          <TextContent>
+                            <Text component="h3">
+                              About the attack
+                            </Text>
+                            <Text component="p" style={{ textAlign: "justify" }}>
+                              {attackDescription}
+                            </Text>
+                          </TextContent>
+
+                          <Button style={{ marginTop: 16 }} variant="primary" isLarge onClick={() => setActiveTab(1)}>
+                            Try it yourself <ArrowRightIcon />
+                          </Button>
+                        </Tab>
+                        <Tab eventKey={1} title="Task">
+                          <TextContent>
+                            <Text component="h3">
+                              Try it yourself
+                            </Text>
+                            <Text component="p" style={{ textAlign: "justify" }}>
+                              {tryItYourself}
+                            </Text>
+                          </TextContent>
+
+                          <Button style={{ marginTop: 16 }} variant="primary" isLarge onClick={() => setActiveTab(2)}>
+                            See the solution <ArrowRightIcon />
+                          </Button>
+                        </Tab>
+                        <Tab eventKey={2} title="Solution">
+                          <TextContent>
+                            <Text component="h3">
+                              Solution
+                            </Text>
+                            <Text component="p" style={{ textAlign: "justify" }}>
+                              {solution}
+                            </Text>
+                          </TextContent>
+
+                          <Button style={{ marginTop: 16 }} variant="primary" isLarge onClick={() => setActiveTab(3)}>
+                            Learn how to prevent <ArrowRightIcon />
+                          </Button>
+                        </Tab>
+                        <Tab eventKey={3} title="Prevention">
+                          <TextContent>
+                            <Text component="h3">
+                              How to prevent
+                            </Text>
+                            <Text component="p" style={{ textAlign: "justify" }}>
+                              {howToPrevent}
+                            </Text>
+                          </TextContent>
+                        </Tab>
+                      </Tabs>
                     </CardBody>
                   </Card>
                 </SplitItem>
--- src/components/Validation/Validation.js
+++ src/components/Validation/Validation.js
@@ -1,6 +1,20 @@
+import { Alert, Card, CardBody, Text, TextContent } from '@patternfly/react-core';
 import React from 'react';
 
-const Validation = () =>
-    <span>Rekreácia WISu, kde sa registrujú predmety a tlačidlá sa odomknú až zajtra</span>
+// Rekreácia WISu, kde sa registrujú predmety a tlačidlá sa odomknú až zajtra
+const Validation = () => {
+    return (
+        <Card>
+            <CardBody>
+                <TextContent>
+                    <Text component="h1">
+                        Project
+                    </Text>
+                </TextContent>
+                <Alert style={{ marginTop: 16 }} variant="danger" isInline title="Project variant selection is unlocking tomorrow at 8:00 PM" />
+            </CardBody>
+        </Card>
+    )
+}
 
 export default Validation;
