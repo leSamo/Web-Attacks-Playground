4e9f64a9e6ccceb19e3203c298f09b1c52b81ced
leSamo <soleksak@redhat.com>
2022-11-20 18:28:01 +0100

    XSS improvements

--- src/components/Xss/Comment.js
+++ src/components/Xss/Comment.js
@@ -2,7 +2,7 @@ import { Card, CardBody, Split, SplitItem } from '@patternfly/react-core';
 import React from 'react';
 import RatingPanel from './RatingPanel';
 
-const Comment = ({ username, content, points, hasUpvoted, hasDownvoted, voteUp, voteDown }) => {
+const Comment = ({ username, content, points, hasUpvoted, hasDownvoted, voteUp, voteDown, date }) => {
     return (
         <Card style={{ marginBottom: 16 }}>
             <CardBody>
@@ -14,7 +14,7 @@ const Comment = ({ username, content, points, hasUpvoted, hasDownvoted, voteUp, 
                         <Split>
                             <SplitItem>
                                 {username}
-                                <span style={{ color: 'gray' }}> • {new Date().toLocaleString("cs-CZ")}</span> <br />
+                                <span style={{ color: 'gray' }}> • {date ? new Date(date).toLocaleString("cs-CZ") : "Just now"}</span> <br />
                                 <p style={{ marginTop: 8, fontSize: 16 }}>{content}</p>
                             </SplitItem>
                             <SplitItem isFilled />
--- src/components/Xss/Xss.js
+++ src/components/Xss/Xss.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Card, CardBody, Stack, StackItem, Text, TextArea, TextContent, TextVariants } from '@patternfly/react-core';
+import { Alert, Button, Card, CardBody, Stack, StackItem, Text, TextArea, TextContent, TextVariants } from '@patternfly/react-core';
 import Comment from './Comment';
 import VulnerableHtmlInjector from 'dangerously-set-html-content'
 
@@ -7,17 +7,19 @@ const Xss = () => {
     const [newCommentValue, setNewCommentValue] = useState("");
 
     const [comments, setComments] = useState([{
-        username: "User 1",
-        content: "This is a comment",
-        points: 6,
+        username: "Linuxer1337",
+        content: "I'd just like to interject for a moment. What you're refering to as <b>Linux</b>, is in fact, <b>GNU/Linux</b>, or as I've recently taken to calling it, GNU plus <b>Linux</b>. <b>Linux</b> is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. \n\n Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called <b>Linux</b>, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.",
+        points: -6,
         hasUpvoted: false,
-        hasDownvoted: false
+        hasDownvoted: false,
+        date: "Mon Nov 18 2022 18:15:28 GMT+0100 (Central European Standard Time)"
     }, {
-        username: "User 2",
-        content: "This is <b>another</b> comment",
-        points: 3,
+        username: "Josh",
+        content: "I couldn't agree more",
+        points: -1,
         hasUpvoted: false,
-        hasDownvoted: false
+        hasDownvoted: false,
+        date: "Mon Nov 19 2022 12:39:01 GMT+0100 (Central European Standard Time)"
     }])
 
     const addNewComment = () => {
@@ -41,36 +43,43 @@ const Xss = () => {
                     <Text component="h1">
                         Discussion
                     </Text>
-                    <Stack hasGutter>
-                        {comments.map(({ username, content, points, hasUpvoted, hasDownvoted }, index) =>
-                            <Comment
-                                key={index}
-                                username={username}
-                                content={<VulnerableHtmlInjector html={content} />}
-                                points={points + Number(hasUpvoted) - Number(hasDownvoted)}
-                                hasDownvoted={hasDownvoted}
-                                hasUpvoted={hasUpvoted}
-                                voteUp={() => setComments(Object.assign([], comments, { [index]: { ...comments[index], hasUpvoted: true, hasDownvoted: false } }))}
-                                voteDown={() => setComments(Object.assign([], comments, { [index]: { ...comments[index], hasUpvoted: false, hasDownvoted: true } }))}
-                            />
-                        )}
+                </TextContent>
+                <Stack hasGutter>
+                    {comments.map(({ username, content, points, hasUpvoted, hasDownvoted, date }, index) =>
+                        <Comment
+                            key={index}
+                            username={username}
+                            content={<VulnerableHtmlInjector html={content} />}
+                            points={points + Number(hasUpvoted) - Number(hasDownvoted)}
+                            hasDownvoted={hasDownvoted}
+                            hasUpvoted={hasUpvoted}
+                            voteUp={() => setComments(Object.assign([], comments, { [index]: { ...comments[index], hasUpvoted: true, hasDownvoted: false } }))}
+                            voteDown={() => setComments(Object.assign([], comments, { [index]: { ...comments[index], hasUpvoted: false, hasDownvoted: true } }))}
+                            date={date}
+                        />
+                    )}
 
-                        <StackItem style={{ marginTop: 32 }}>
+                    <StackItem style={{ marginTop: 32 }}>
+                        <TextContent>
                             <Text component={TextVariants.h6}>
                                 Add a new comment
                             </Text>
-                            <TextArea
-                                value={newCommentValue}
-                                onChange={value => setNewCommentValue(value)}
-                                resizeOrientation="vertical"
-                                aria-label="comment"
-                                style={{ minHeight: 60 }}
-                                placeholder="Type comment..."
-                            />
-                            <Button style={{ marginTop: 16 }} variant="primary" isDisabled={newCommentValue.length === 0} onClick={addNewComment}>Submit comment</Button>
-                        </StackItem>
-                    </Stack>
-                </TextContent>
+                        </TextContent>
+
+                        <Alert style={{ marginTop: 16, marginBottom: 16 }} variant="info" isInline title="New: You can now use HTML tags to format your messages!" />
+
+                        <TextArea
+                            value={newCommentValue}
+                            onChange={value => setNewCommentValue(value)}
+                            resizeOrientation="vertical"
+                            aria-label="comment"
+                            style={{ minHeight: 60 }}
+                            placeholder="Type comment..."
+                        />
+                        <Button style={{ marginTop: 16 }} variant="primary" isDisabled={newCommentValue.length === 0} onClick={addNewComment}>Submit comment</Button>
+                    </StackItem>
+                </Stack>
+
             </CardBody>
         </Card>
     );
